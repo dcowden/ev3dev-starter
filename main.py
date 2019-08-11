f6b9c4c4b9e16a9059c0fdb11eec2a70d4433f95
Dave Cowden <dave.cowden@gmail.com>
2019-08-11 15:44:14 -0400

    added differential drive

--- main.py
+++ main.py
@@ -3,8 +3,23 @@ from ev3dev2.motor import *
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import TouchSensor
 from ev3dev2.led import Leds
-
+from ev3dev2.wheel import Wheel
 #see https://python-ev3dev.readthedocs.io/en/ev3dev-stretch/motors.html
-robot = MoveSteering(OUTPUT_B, OUTPUT_C)
 
-robot.on_for_rotations(0,SpeedPercent(25),2, brake=True,block=True)
+
+class BaloonWheel(Wheel):
+    def __init__(self):
+        Wheel.__init__(self,56,28)
+
+def inches_to_mm(inches):
+    return inches * 25.4;
+
+STUD_MM=8
+
+robot = MoveDifferential(OUTPUT_B, OUTPUT_C, BaloonWheel, 14* STUD_MM)
+
+SPEED=40
+
+robot.on_for_distance(SpeedPercent(SPEED),inches_to_mm(12),brake=False,block=True)
+robot.turn_right(SPEED,90,brake=True,block=True)
+robot.on_for_distance(SpeedPercent(SPEED),inches_to_mm(24),brake=True,block=True)
